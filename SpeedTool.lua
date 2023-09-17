579b8f58487c2d4b53c78d7047dfc44bf142ea37
laixy <125830449+laixy1337@users.noreply.github.com>
2023-09-18 00:42:12 +0300

    Update SpeedTool.lua

--- SpeedTool.lua
+++ SpeedTool.lua
@@ -1,7 +1,7 @@
 tool = Instance.new("Tool")
 tool.RequiresHandle = false
 tool.Name = "Speed Tool"
-tool.Equipped:connect(function()
+tool.Activated:connect(function()
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
 end)
 tool.Parent = game.Players.LocalPlayer.Backpack
