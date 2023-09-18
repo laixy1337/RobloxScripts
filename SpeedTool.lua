4de2d93c5032ab20207a135465974562890d3989
laixy <125830449+laixy1337@users.noreply.github.com>
2023-09-18 16:27:34 +0300

    Update SpeedTool.lua

--- SpeedTool.lua
+++ SpeedTool.lua
@@ -2,11 +2,12 @@ tool = Instance.new("Tool")
 tool.RequiresHandle = false
 tool.Name = "Speed Tool"
 tool.Equipped:Connect(function()
-    while tool.Unequipped == true do
+    while wait() do
       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 150
+    end
 end)
 tool.Unequipped:Connect(function()
-    while tool.Equipped == false do
+    while wait() do
       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
     end
 end)
