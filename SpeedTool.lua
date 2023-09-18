fa1ac2820cf1dafb4b1be939cbea25bddaca8dd8
laixy <125830449+laixy1337@users.noreply.github.com>
2023-09-18 16:36:17 +0300

    Update SpeedTool.lua

--- SpeedTool.lua
+++ SpeedTool.lua
@@ -5,8 +5,8 @@ tool.Equipped:Connect(function()
     while wait() do
       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 150
     end
+end)
 tool.Unequipped:Connect(function()
     break
 end)
-    end)
 tool.Parent = game.Players.LocalPlayer.Backpack
